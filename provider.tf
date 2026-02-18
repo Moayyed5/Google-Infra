8a9a6c1515e52ad145bcbfb6f5e87e48319cd7b5
Moayyed5 <93940346+Moayyed5@users.noreply.github.com>
2026-02-18 04:00:29 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "1.14.5"
+  required_version = "1.0.0"
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "6.47.0"
+      version = "7.19.0"
     }
   }
 }
